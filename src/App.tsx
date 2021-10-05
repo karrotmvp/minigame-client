00970efc33bb0c415c9aec24aa1c77ec8c708985
jongwooha98 <jongwooha98@gmail.com>
2021-10-05 20:59:04 +0900

    feat: Add Karrot Market Javascript SDK (Mini)
    - Update appEjectionButton that now closes the mini-app and goes back to main Karrot Market app
    - Add 간편 동의 프리셋 and show NewUserHome screen to new user
    -

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,36 @@
 import { css } from '@emotion/react';
 import '@karrotframe/navigator/index.css';
 import Home from './pages/Home';
+import NewUserHome from './pages/NewUserHome';
 import Game from './pages/Game';
 import Leaderboard from './pages/Leaderboard';
 import { Navigator, Screen } from '@karrotframe/navigator';
+import { getMini } from 'api/mini';
+import { useState } from 'react';
 
 const appStyle = css`
   height: 100vh;
 `;
 
-// import Mini from '@karrotmarket/mini';
-//
 function App() {
-  // const mini = new Mini();
+  const [isNewUser, setIsNewUser] = useState<boolean>(false);
+
+  const handleAppEjection = () => {
+    const mini = getMini();
+    mini.close();
+  };
+
   return (
     <div css={appStyle}>
       <Navigator
         theme="Cupertino"
         onClose={() => {
-          console.log('닫기버튼이 눌렸습니다');
+          console.log(`${handleAppEjection} 닫기버튼이 눌렸습니다`);
         }}
       >
-        <Screen path="/" component={Home} />
+        <Screen path="/">
+          {isNewUser ? <NewUserHome setIsNewUser={setIsNewUser} /> : <Home />}
+        </Screen>
         <Screen path="/game" component={Game} />
         <Screen path="/leaderboard" component={Leaderboard} />
       </Navigator>
--- src/components/AppEjectionButton.tsx
+++ src/components/AppEjectionButton.tsx
@@ -1,7 +1,10 @@
+import { getMini } from 'api/mini';
 import IconClose from 'assets/IconClose';
 
 const AppEjectionButton = () => {
+  const mini = getMini();
   const handleAppEjection = () => {
+    mini.close();
     console.log('Ejected from the app. Now back to Karrot Market');
   };
   return (
