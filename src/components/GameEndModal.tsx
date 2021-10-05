21bd2e3de11ec536433378d46a0e2591337542b5
jongwooha98 <jongwooha98@gmail.com>
2021-10-05 12:16:01 +0900

    Feat: Update GameEndModal
    - Update div style

--- src/components/GameEndModal.tsx
+++ src/components/GameEndModal.tsx
@@ -6,19 +6,32 @@ import { emphasizedTextStyle, largeTextStyle } from 'styles/textStyle';
 import IndividualLeaderboard from './IndividualLeaderboard';
 import { sampleUserData } from 'sampleUserData';
 import Button from './Button';
-import { NONAME } from 'dns';
 
 interface CloseButtonProps {
   handleClose: () => void;
 }
 const CloseButton = ({ handleClose }: CloseButtonProps) => {
   return (
-    <a onClick={handleClose}>
+    <a
+      onClick={handleClose}
+      style={{ display: 'flex', justifyContent: 'flex-end' }}
+    >
       <IconClose />
     </a>
   );
 };
 
+const modalStyle = css`
+  position: absolute;
+  background: #fff;
+  top: 25px;
+  inset: 10% 8% 10%;
+  padding: 15px;
+  border-radius: 10px;
+  display: flex;
+  flex-flow: column;
+  overflow: scroll;
+`;
 interface GameEndModalProps {
   handleCloseModal: () => void;
   score: number;
@@ -39,36 +52,34 @@ const GameEndModal = ({ handleCloseModal, score }: GameEndModalProps) => {
         backdropFilter: 'blur(3px)',
       }}
     >
-      <div
-        style={{
-          position: 'absolute',
-          background: '#fff',
-          top: '25px',
-
-          inset: '10% 8% 10%',
-          padding: 15,
-          borderRadius: '10px',
-        }}
-      >
+      <div css={modalStyle}>
         <CloseButton handleClose={handleCloseModal} />
         <div
           style={{
             display: 'flex',
             flexFlow: 'column',
+            height: '95%',
           }}
         >
-          <h1 css={largeTextStyle} style={{ textAlign: 'center' }}>
+          <h1
+            css={largeTextStyle}
+            style={{ textAlign: 'center', flex: '0 1 auto' }}
+          >
             <span css={emphasizedTextStyle}>{score}개</span>의 당근을
             <br />
             수확했어요!
           </h1>
-          <IndividualLeaderboard userData={sampleUserData} />
+          <div style={{ overflow: 'hidden', flex: '1 1 auto' }}>
+            <IndividualLeaderboard userData={sampleUserData} />
+          </div>
           <div
             style={{
               width: `100%`,
               display: `flex`,
+              flex: '0 1 40px',
               justifyContent: `space-evenly`,
-              padding: `20px 0`,
+              paddingTop: `20px`,
+              gap: '10px',
             }}
           >
             <Button
