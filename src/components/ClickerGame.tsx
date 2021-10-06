6474af8fdfcad1a219cddd80402cd5be342e4919
jongwooha98 <jongwooha98@gmail.com>
2021-10-06 16:08:08 +0900

    feat: Add click effect

--- src/components/ClickerGame.tsx
+++ src/components/ClickerGame.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-
 const fullScreen = css`
   height: 100%;
   position: absolute;
@@ -9,12 +8,50 @@ const fullScreen = css`
   overflow: hidden;
 `;
 
-function ClickerGame({ score, onIncrease }: any) {
+interface ClickerGameProps {
+  score: number;
+  onIncrease: any;
+}
+
+const ClickerGame = ({ score, onIncrease }: ClickerGameProps) => {
+  const gameContainer = document.getElementById('clicker-game__container');
+
+  const clickerAnimation = async ({ posX, posY }: any) => {
+    let karrot = document.createElement('div');
+    karrot.className = `karrot`;
+    karrot.style.position = `absolute`;
+    karrot.style.width = `30px`;
+    karrot.style.height = `30px`;
+    karrot.style.backgroundColor = 'hotpink';
+
+    let coords = `x pos: ${posX} & y pos: ${posY}`;
+    console.log(coords);
+
+    karrot.style.left = `${posX}px`;
+    karrot.style.top = `${posY}px`;
+    let karrotImage = document.createElement('img');
+    karrotImage.src = 'src/assets/karrot.png';
+
+    karrot.appendChild(karrotImage);
+    gameContainer?.appendChild(karrot);
+
+    setTimeout(() => {
+      karrot.remove();
+    }, 10000);
+    console.log('works');
+  };
+
+  const handleClick = async (e: any) => {
+    let posX = e.clientX;
+    let posY = e.clientY;
+    await onIncrease();
+    await clickerAnimation({ posX, posY });
+  };
   return (
-    <div css={fullScreen} onClick={onIncrease}>
+    <div id="clicker-game__container" css={fullScreen} onClick={handleClick}>
       <h1>{score}</h1>
     </div>
   );
-}
+};
 
 export default ClickerGame;
