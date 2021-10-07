3395a8023d20bf0feaa093aebae655fc9b9c0fb5
jongwooha98 <jongwooha98@gmail.com>
2021-10-07 22:52:48 +0900

    feat: Add custom nav

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import IconBack from '../assets/IconBack';
+import { AppEjectionButton } from './AppEjectionButton';
+const customNav = css`
+  left: 0;
+  width: 100%;
+  // height: 100%;
+  top: 0;
+  display: flex;
+  width: 100%;
+  height: 2.75rem;
+  padding: 0 0.5rem;
+`;
+
+const custonNavIcon = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  opacity: 1;
+  transition: opacity 300ms;
+  width: 2.25rem;
+  height: 2.75rem;
+  text-decoration: none;
+  outline: none;
+  z-index: 10;
+`;
+
+interface TopNavprops {
+  action: string;
+  handleNavCloseAction?: () => void;
+  handleNavBackAction?: () => void;
+}
+const TopNav = ({
+  action,
+  handleNavCloseAction,
+  handleNavBackAction,
+}: TopNavprops) => {
+  return (
+    <div css={customNav}>
+      <div css={custonNavIcon}>
+        {action === `eject` ? (
+          <AppEjectionButton />
+        ) : action === `back` ? (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={handleNavBackAction}>
+            <IconBack />
+          </a>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
