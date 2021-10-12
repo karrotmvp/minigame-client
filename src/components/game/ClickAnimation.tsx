81700ebb74da8491e49fba9e30ed2712e94eb7a6
jongwooha98 <jongwooha98@gmail.com>
2021-10-12 21:17:27 +0900

    feat: Add click effect

--- src/components/game/ClickAnimation.tsx
+++ src/components/game/ClickAnimation.tsx
@@ -1,32 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
 import { ReactComponent as Karrot } from 'assets/karrot.svg';
-import './clickAnimation.css';
 
-interface ExpireProps {
+interface karrotStyleProps {
   posX: number;
   posY: number;
-  visible: boolean;
 }
-const ClickAnimation = ({ posX, posY, visible }: ExpireProps) => {
-  // let b = Math.floor(Math.random() * 100 + 1);
-  // let d = ['flowOne', 'flowTwo', 'flowThree'];
-  // let a = ['colOne', 'colTwo', 'colThree', 'colFour', 'colFive', 'colSix'];
-  // let c = (Math.random() * (1.6 - 1.2) + 1.2).toFixed(1);
+const karrotStyle = ({ posX, posY }: karrotStyleProps) => css`
+  left: ${posX}px;
+  top: ${posY}px;
+  position: absolute;
+  z-index: 10000;
+`;
+interface ClickAnimationProps {
+  posX: number;
+  posY: number;
+}
+const ClickAnimation = ({ posX, posY }: ClickAnimationProps) => {
   return (
     <>
-      {visible ? (
-        <Karrot
-          // className={`${a[Math.floor(Math.random() * 6)]}`}
-          style={{
-            position: 'absolute',
-            left: `${posX}px`,
-            top: `${posY}px`,
-            // width: `${Math.floor(Math.random() * (50 - 22) + 22)}px`,
-            // height: `auto`,
-            // animation: `${d[0]} ${c}s linear`,
-            // [Math.floor(Math.random() * 3)]
-          }}
-        />
-      ) : null}
+      <Karrot css={karrotStyle({ posX, posY })} />
     </>
   );
 };
