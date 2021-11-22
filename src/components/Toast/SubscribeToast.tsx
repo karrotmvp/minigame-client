3989b11c8ec8f1ba16802b7a493846fd80c6bf4f
jongwooha98 <jongwooha98@gmail.com>
2021-11-22 17:45:55 +0900

    update: Toast design

--- src/components/Toast/SubscribeToast.tsx
+++ src/components/Toast/SubscribeToast.tsx
@@ -10,29 +10,49 @@ export const SubscribeToastContainer = () => {
       limit={1}
       transition={Slide}
       className={css`
-        .Toastify__toast--success {
+        .Toastify__toast--default {
           bottom: 30px;
           width: fit-content;
           height: fit-content;
           min-height: fit-content;
           margin: auto;
           padding: 10px 18px;
+          background: #5b5b5b;
+          opacity: 0.9;
           border-radius: 10px;
-          background: rgba(0, 0, 0, 0.6);
           box-shadow: none;
         }
         .Toastify__toast-body {
           font-family: Pretendard;
           font-style: normal;
           font-weight: normal;
-          font-size: 14px;
+
           line-height: 161.7%;
           height: fit-content;
+          text-align: center;
           color: #ffffff;
           margin: 0;
           padding: 0;
-          div {
+
+          p {
+            font-style: normal;
+            font-weight: normal;
+            font-size: 16px;
+            line-height: 161.7%;
+            /* identical to box height, or 26px */
+
+            color: #ffc8a9;
+          }
+          span {
+            font-style: normal;
+            font-weight: normal;
             font-size: 14px;
+            line-height: 161.7%;
+            /* identical to box height, or 23px */
+
+            /* gray/gray 200 */
+
+            color: #ececec;
           }
         }
       `}
@@ -41,11 +61,32 @@ export const SubscribeToastContainer = () => {
 };
 
 export const subscribeToastEmitter = () => {
-  toast.success('즐겨찾기 성공', {
-    icon: <img src={checkMarkUrl} alt="" />,
-    position: 'bottom-center',
-    autoClose: 3000,
-    delay: 1000,
-    hideProgressBar: true,
-  });
+  toast(
+    <p>
+      <img
+        src={checkMarkUrl}
+        alt=""
+        style={{
+          marginRight: `8px`,
+        }}
+      />
+      구독 추가 완료!
+      <br />
+      <span
+        style={{
+          fontSize: `14px`,
+          lineHeight: `161.7%`,
+          color: `#ECECEC`,
+        }}
+      >
+        내 근처 탭에서 확인할 수 있어요
+      </span>
+    </p>,
+    {
+      position: 'bottom-center',
+      autoClose: 3000,
+      delay: 200,
+      hideProgressBar: true,
+    }
+  );
 };
