6f01598697308696b74a8d5a4d9523f82c7e76a4
jongwooha98 <jongwooha98@gmail.com>
2021-11-28 02:10:45 +0900

    update survey api

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { Nav } from 'components/Navigation/Nav';
 import { BackIcon } from 'assets/Icon';
-import { useNavigator } from '@karrotframe/navigator';
+import { useCurrentScreen, useNavigator } from '@karrotframe/navigator';
 import { rem } from 'polished';
 import { color, PageContainer } from 'styles';
 import { Button } from 'components/Button';
@@ -14,6 +14,7 @@ import { useUser } from 'redux/user';
 
 export const Survey: React.FC = () => {
   const analytics = useAnalytics();
+  const { isTop } = useCurrentScreen();
   const { pop } = useNavigator();
   const minigameApi = useMinigameApi();
   const { regionId } = useUserData();
@@ -29,22 +30,52 @@ export const Survey: React.FC = () => {
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGameSurveyInput(e.target.value);
   };
+
+  const postGameSurvey = async ({
+    uuid,
+    regionId,
+    content,
+  }: {
+    uuid: string;
+    regionId: string;
+    content: string;
+  }) => {
+    try {
+      const { data } = await minigameApi.surveyApi.registerGameSurveyUsingPOST(
+        uuid,
+        regionId,
+        {
+          content,
+        }
+      );
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const submitGameSurvey = async () => {
     inputRef.current.focus();
-    const { data } = await minigameApi.surveyApi.registerGameSurveyUsingPOST(
-      uuid,
-      regionId,
-      {
-        content: gameSurveyInput,
-      }
-    );
-    if (data.status === 200) {
+    const response = await postGameSurvey({
+      uuid: uuid,
+      regionId: regionId,
+      content: gameSurveyInput,
+    });
+    if (response?.status === 200) {
       analytics.logEvent('click_submit_game_request_button');
       setGameSurveyInput('');
       surveyToastEmitter();
     }
   };
 
+  useEffect(() => {
+    if (isTop) {
+      const timerId = setTimeout(() => {
+        inputRef.current.focus();
+      }, 300);
+      return clearTimeout(timerId);
+    }
+  }, [isTop]);
   return (
     <>
       <Nav appendLeft={<BackIcon />} onClickLeft={goToPlatformPage} />
@@ -53,7 +84,6 @@ export const Survey: React.FC = () => {
           <h3>하고 싶은 게임이 있나요?</h3>
           <div>
             <input
-              autoFocus
               ref={inputRef}
               type="text"
               onChange={handleInputChange}
